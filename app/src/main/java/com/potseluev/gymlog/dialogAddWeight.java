7dc4a964d0c8b73eef61ca43a9cd6192b311b001
evgeniy.potseluev <evgeniy.potseluev@gmail.com>
2017-12-13 22:42:41 +0300

    first

--- app/src/main/java/com/potseluev/gymlog/dialogAddWeight.java
+++ app/src/main/java/com/potseluev/gymlog/dialogAddWeight.java
@@ -1,9 +1,7 @@
 package com.potseluev.gymlog;
 
 import android.app.DialogFragment;
-import android.content.ContentValues;
 import android.content.DialogInterface;
-import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.text.InputFilter;
 import android.util.Log;
@@ -29,10 +27,8 @@ public class dialogAddWeight extends DialogFragment implements OnClickListener {
     Button btnMinus05;
 
     DBHelper dbhelper;
-    SQLiteDatabase db;
-    ContentValues cv;
     Date date;
-    InputFilter inputFilter; //HELLO
+    InputFilter inputFilter;
 
     public View onCreateView(LayoutInflater inflater, final ViewGroup container,
                              Bundle savedInstanceState) {
@@ -40,20 +36,18 @@ public class dialogAddWeight extends DialogFragment implements OnClickListener {
         View v = inflater.inflate(R.layout.dialog_weight_add, null);
         v.findViewById(R.id.btnYes).setOnClickListener(this);
 
+        //Элементы экрана
         editWeightValue = v.findViewById(R.id.editWeightValue);
         btnMinus05 = v.findViewById(R.id.btnMinus05);
         btnPlus05 = v.findViewById(R.id.btnPlus05);
-
         dbhelper = new DBHelper(v.getContext());
-        db = dbhelper.getWritableDatabase();
-        cv = new ContentValues(1);
-
+        //Только цифровой ввод веса пользователя
         editWeightValue.setFilters(new InputFilter[]{new DecimalDigitsInputFilter(3, 1)});
-//        editWeightValue.setSelectAllOnFocus(true);
 
-        double value = dbhelper.getLastSavedWeight();
-        if (value > 0) {
-            editWeightValue.setText(String.valueOf(value));
+        //Покажем последнее сохраненное значение веса
+        try {
+            editWeightValue.setText(String.valueOf(dbhelper.getLastSavedWeight()));
+        } catch (Exception ignored) {
         }
 
         OnClickListener onClickListener = new OnClickListener() {
@@ -62,42 +56,36 @@ public class dialogAddWeight extends DialogFragment implements OnClickListener {
                 double currentValue = 0;
                 try {
                     currentValue = Double.parseDouble(String.valueOf(editWeightValue.getText()));
-                } catch (Exception e) {
+                } catch (Exception ignored) {
                 }
 
-                double newValue = currentValue;
                 switch (view.getId()) {
                     case R.id.btnPlus05:
-                        newValue = (currentValue + 0.5);
+                        editWeightValue.setText(String.valueOf(currentValue + 0.5));
                         break;
                     case R.id.btnMinus05:
-                        newValue = (currentValue - 0.5);
+                        editWeightValue.setText(String.valueOf(currentValue - 0.5));
                         break;
                 }
-                editWeightValue.setText(String.valueOf(newValue));
             }
         };
 
         btnMinus05.setOnClickListener(onClickListener);
         btnPlus05.setOnClickListener(onClickListener);
-
         return v;
     }
 
     public void onClick(View v) {
-        Log.d("mytag", "Dialog 1: " + ((Button) v).getText());
         saveWeightToDB(v);
         dismiss();
     }
 
     public void onDismiss(DialogInterface dialog) {
         super.onDismiss(dialog);
-        Log.d("mytag", "Dialog 1: onDismiss");
     }
 
     public void onCancel(DialogInterface dialog) {
         super.onCancel(dialog);
-        Log.d("mytag", "Dialog 1: onCancel");
     }
 
 
@@ -109,13 +97,11 @@ public class dialogAddWeight extends DialogFragment implements OnClickListener {
             Toast.makeText(v.getContext(), "Error", Toast.LENGTH_SHORT).show();
         }
         double value = Double.parseDouble(String.valueOf(editWeightValue.getText()));
-        date = Calendar.getInstance().getTime();
         SimpleDateFormat fmt = new SimpleDateFormat("yyyy-MM-dd");
-        String Date = fmt.format(date);
+        String Date = fmt.format(Calendar.getInstance().getTime());
 
         long newRow = dbhelper.addWeight(Date, value);
         if (newRow > 0) {
-
             Toast.makeText(v.getContext(), "Разница: " + String.valueOf(value - previousValue), Toast.LENGTH_SHORT).show();
         }
     }
