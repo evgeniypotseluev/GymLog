ccb91e8e00f2d562e0b50a38e8e5ae2b1332f50c
evgeniy.potseluev <evgeniy.potseluev@gmail.com>
2017-12-13 22:24:11 +0300

    first

---
+++ app/src/main/java/com/potseluev/gymlog/Constants.java
@@ -0,0 +1,17 @@
+package com.potseluev.gymlog;
+
+/**
+ * Created by evgen on 30.10.2017.
+ */
+
+final class Constants {
+    static String PREFERENCE_FILE = "GymLog_SharedPreferences";
+
+    static String WELCOME_ACTIVITY_STATUS = "WelcomeActivityStatus";
+    static int WELCOME_ACTIVITY_STATUS_INITIAL = 0;
+    static int WELCOME_ACTIVITY_STATUS_NOT_FINISHED = 1;
+    static int WELCOME_ACTIVITY_STATUS_FULLY_FINISHED = 2;
+
+
+
+}
